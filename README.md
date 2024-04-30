1e288eed6327a2bfb56dee2cce8530c481a480fd
MASSELIN Arnold <144697843+ArnoldMsl@users.noreply.github.com>
2024-04-30 11:50:21 +0200

    Initialize application with Vite + React and implement base mirroring feature

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Minis-printer
-Minis-printer application
+
+Minis-printer is a single page application to create D&D characters display.
+
+## Summary
+
+Provided by the Hoard, this single page React application provides an environment for the user to create Minis to be used for his RPG games.
+
+To do so, the user can upload his characters picture and crop it, the image will then be edited in order to create a cuttable mini.
+
+The edited mini can then be downloaded by the user and/or incorporated into a collection of different minis on an A4 format canvas which can be downloaded for the user to print and cut.
+
+
+## Features
+
+The user can : 
+* Upload his own picture.
+* Crop it.
+* Choose the dimensions of the Mini according to D&D standards.
+* Edit the style and dimensions of the Minis base.
+* Add a single or many edited Mini to an A4 format canvas.
+* Start again with a different picture that can be added to the previous A4 canvas.
+* Download an individual Mini or the whole Canvas.
+
---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [imagePreview, setImagePreview] = useState(null);
+
+  const handleImageChange = (imageChange) => {
+    imageChange.preventDefault();
+
+    const file = imageChange.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      const img = new Image();
+      img.onload = () => {
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+
+        canvas.width = img.width;
+        canvas.height = (img.height * 2) + 10;
+
+        ctx.save();
+        ctx.scale(1, -1);
+        ctx.drawImage(img, 0, -img.height);
+        ctx.restore();
+
+        ctx.drawImage(img, 0, (img.height) + 10);
+
+        const dataURL = canvas.toDataURL('image/jpeg');
+
+        setImagePreview(dataURL);
+
+      };
+      img.src = event.target.result;
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Minis-printer</h1>
+      <input type="file" onChange={handleImageChange} accept="image/*" />
+      {imagePreview && (
+        <div>
+          <h2>Prévisualisation de l'image :</h2>
+          <img src={imagePreview} alt="Prévisualisation" style={{ maxWidth: '100%', maxHeight: '400px' }} />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
