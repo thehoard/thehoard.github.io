e8ad78eda53d4cb6d87e4a44c1d04f2301ca257d
ArnoldMsl <arnold.masselin@gmail.com>
2024-05-02 17:23:42 +0200

    commit before implementing base width calculation into ImageEditor

--- src/components/ArmyContainer.jsx
+++ src/components/ArmyContainer.jsx
@@ -12,12 +12,17 @@ function ArmyContainer({ army }) {
         // Dimensions d'une feuille A4 en mm
         const A4_WIDTH_MM = 210
         const A4_HEIGHT_MM = 297
+        const SQUARE_MM = 25.4
 
         // Convertir les dimensions en pixels en fonction de la densité de pixels de l'écran
         const dpi = window.devicePixelRatio || 1
         const A4_WIDTH_PX = Math.floor(A4_WIDTH_MM * dpi * 3.7795) // 1mm = 3.7795 pixels
         const A4_HEIGHT_PX = Math.floor(A4_HEIGHT_MM * dpi * 3.7795) // 1mm = 3.7795 pixels
-        console.log(A4_HEIGHT_PX)
+        const SQUARE_PX = Math.floor(SQUARE_MM * dpi * 3.7795)
+        const TOTAL_SQUARES_A4 = Math.floor((A4_WIDTH_PX / SQUARE_PX) * (A4_HEIGHT_PX / SQUARE_PX))
+        console.log("A4 hauteur en pixels : ", A4_HEIGHT_PX)
+        console.log("carré en pixels : ", SQUARE_PX)
+        console.log("nombre total de carrés : ", TOTAL_SQUARES_A4)
 
         // Définir la taille du canvas en pixels
         canvas.width = A4_WIDTH_PX
@@ -27,33 +32,28 @@ function ArmyContainer({ army }) {
         ctx.strokeStyle = 'black'
         ctx.strokeRect(0, 0, A4_WIDTH_PX, A4_HEIGHT_PX)
 
-        const resizeAndDrawImage = (image) => { //fonction de redimensionnement et dessin de l'image par rapport à la hauteur
+        const resizeAndDrawImage = (image) => { //fonction de redimensionnement et dessin de l'image par rapport aux dimensions
+
             const aspectRatio = image.width / image.height
-            const resizedImageHeight = A4_HEIGHT_PX; // On calcule les futures dimensions de l'image
+            const resizedImageHeight = A4_HEIGHT_PX; // On calcule les futures dimensions de l'image pour définir si elle est placée verticalement ou horizontalement
             const resizedImageWidth = (resizedImageHeight * aspectRatio)
 
-            console.log("future largeur : ", resizedImageWidth)
-            console.log("future hauteur : ", resizedImageWidth)
-
-            console.log("hauteur naturelle : ", image.height)
             console.log("largeur naturelle : ", image.width)
-            console.log("aspect ratio : ", aspectRatio)
+            console.log("future largeur : ", resizedImageWidth)
+            console.log("largeur A4 : ", A4_WIDTH_PX)
 
             if (resizedImageWidth > A4_WIDTH_PX) { // Si la future largeur de l'image dépasse celle du Canvas alors on tourne l'image de 90deg
+
                 image.height = A4_WIDTH_PX
                 image.width = image.height
-                // console.log("rotation")
-                // console.log("hauteur modifiée : ", image.height)
-                // console.log("largeur modifiée: ", image.width)
+                ctx.save() // on enregistre le contexte vertical du canvas
                 ctx.translate(image.width, 0); // Translation pour positionner l'image
                 ctx.rotate(Math.PI / 2); // Rotation de 90 degrés
                 ctx.drawImage(image, 0, 0, image.height, image.width); // Dessin de l'image avec les dimensions modifiées
+                ctx.restore() // on remet le canvas à la verticale après déssiné l'image.
+
             } else { // Sinon on la dessine verticalement
-                image.height = A4_HEIGHT_PX
-                image.width = (image.height * aspectRatio)
-                // console.log("hauteur modifiée : ", image.height)
-                // console.log("largeur modifiée: ", image.width)
-                ctx.drawImage(image, 0, 0, image.height, image.width)
+                ctx.drawImage(image, 0, 0, resizedImageWidth, resizedImageHeight)
             }
         }
 
@@ -63,9 +63,7 @@ function ArmyContainer({ army }) {
                 const image = new Image() // Instanciation d'une image
 
                 image.onload = function () {
-                    ctx.save() // on enregistre le contexte actuel du canvas (à la verticale)
                     resizeAndDrawImage(image)
-                    ctx.restore()
                 }
                 image.src = mini.image
             }
