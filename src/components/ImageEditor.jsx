5c163cacb8f92f454141406c4974b238f091c1b7
ArnoldMsl <arnold.masselin@gmail.com>
2024-05-03 09:46:28 +0200

    reorganize useEffect on imageEditor

--- src/components/ImageEditor.jsx
+++ src/components/ImageEditor.jsx
@@ -8,60 +8,53 @@ function ImageEditor({ imageUrl, onArmyChange }) {
   const [selectedSize, setSelectedSize] = useState(null)
   const [repeatValue, setRepeatValue] = useState(1)
 
-
-
   useEffect(() => {
-    const baseImage = new Image()
-    baseImage.onload = () => { //chargement de l'image de la base
-      setBaseImg(baseImage)
-    }
-    baseImage.src = '../src/assets/images/Minibase.svg'
-  }, [])
-
-  useEffect(() => {
-    // Gestionnaire de la taille du Mini
-    const handleSizeChange = (event) => {
-      setSelectedSize(event.target.value)
-
-    }
-  }
-  )
-
-  useEffect(() => {
-    if (imageUrl && baseImg) {
-      const img = new Image()
-
+    if (imageUrl) {
+      const img = new Image();
       img.onload = () => {
-        const baseWidth = img.width * 0.4 //définition de la taille automatique de la base et des espaces de pliures
-        const baseHeight = img.height * 0.3
-        const foldStroke = 4 // l'espace de pliure est défini à 4 pixels, soit environ 1mm.
-
-        const canvas = document.createElement('canvas')
-        const ctx = canvas.getContext('2d')
-
-        canvas.width = (img.width + baseWidth)
-        canvas.height = ((img.height * 2) + (baseHeight * 2) + (foldStroke * 2))
-        const centerXCanvas = ((canvas.width - img.width) / 2)
-        const centerYCanvas = ((canvas.height - ((img.height * 2) + (foldStroke * 2))) / 2)
-
-        ctx.save()
-        ctx.fillStyle = 'white'
-        ctx.fillRect(0, 0, canvas.width, canvas.height)
-        ctx.scale(1, -1)
-        ctx.drawImage(img, centerXCanvas, (-centerYCanvas - img.height - foldStroke))
-        ctx.restore()
-        ctx.drawImage(img, centerXCanvas, (centerYCanvas + img.height + foldStroke * 2))
-        ctx.drawImage(baseImg, 0, 0, canvas.width, baseHeight)
-        ctx.drawImage(baseImg, 0, (canvas.height - baseHeight + foldStroke), canvas.width, baseHeight)
-
-        const dataURL = canvas.toDataURL('image/jpeg')
-
-        setImagePreview(dataURL)
-      }
-
-      img.src = imageUrl
+        const baseImg = new Image();
+        baseImg.onload = () => { //chargement de l'image de la base
+          setBaseImg(baseImg);
+          drawCanvas(img, baseImg, selectedSize); // Appelez drawCanvas avec img et baseImg
+        };
+        baseImg.src = '../src/assets/images/Minibase.svg'; // Assurez-vous que le chemin est correct
+      };
+      img.src = imageUrl;
     }
-  }, [imageUrl, baseImg])
+  }, [imageUrl, selectedSize]);
+
+
+  const drawCanvas = (img, baseImg, selectedSize) => {
+
+    console.log(selectedSize)
+
+    const baseWidth = img.width * 0.4 //définition de la taille automatique de la base et des espaces de pliures
+    const baseHeight = img.height * 0.3
+    const foldStroke = 4 // l'espace de pliure est défini à 4 pixels, soit environ 1mm.
+
+    const canvas = document.createElement('canvas')
+    const ctx = canvas.getContext('2d')
+
+    canvas.width = (img.width + baseWidth)
+    canvas.height = ((img.height * 2) + (baseHeight * 2) + (foldStroke * 2))
+    const centerXCanvas = ((canvas.width - img.width) / 2)
+    const centerYCanvas = ((canvas.height - ((img.height * 2) + (foldStroke * 2))) / 2)
+
+    ctx.save()
+    ctx.fillStyle = 'white'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx.scale(1, -1)
+    ctx.drawImage(img, centerXCanvas, (-centerYCanvas - img.height - foldStroke))
+    ctx.restore()
+    ctx.drawImage(img, centerXCanvas, (centerYCanvas + img.height + foldStroke * 2))
+    ctx.drawImage(baseImg, 0, 0, canvas.width, baseHeight)
+    ctx.drawImage(baseImg, 0, (canvas.height - baseHeight + foldStroke), canvas.width, baseHeight)
+
+    const dataURL = canvas.toDataURL('image/jpeg')
+
+    setImagePreview(dataURL)
+  }
+
 
   const downloadImage = () => {
     const downloadLink = document.createElement('a')
@@ -89,6 +82,10 @@ function ImageEditor({ imageUrl, onArmyChange }) {
     setRepeatValue(event.target.value)
   }
 
+  const handleSizeChange = (event) => {
+    setSelectedSize(event.target.value)
+  }
+
 
   return (
     <div>
@@ -96,12 +93,12 @@ function ImageEditor({ imageUrl, onArmyChange }) {
         <div id="armyControls">
           <div id="armyButtons">
             <h2>Choix de la taille</h2>
-            <label><input type="radio" name="size" value="0.25" onChange={setSelectedSize} /> Minuscule (60 cm)</label>
-            <label><input type="radio" name="size" value="1" onChange={setSelectedSize} /> Petit (1.2m)</label>
-            <label><input type="radio" name="size" value="1" onChange={setSelectedSize} /> Moyen (2.5m)</label>
-            <label><input type="radio" name="size" value="4" onChange={setSelectedSize} /> Grand (4.5m)</label>
-            <label><input type="radio" name="size" value="9" onChange={setSelectedSize} /> Énorme (9m)</label>
-            <label><input type="radio" name="size" value="16" onChange={setSelectedSize} /> Gargantuesque (9m et +)</label>
+            <label><input type="radio" name="size" value="0.25" onChange={handleSizeChange} /> Minuscule (60 cm)</label>
+            <label><input type="radio" name="size" value="1" onChange={handleSizeChange} /> Petit (1.2m)</label>
+            <label><input type="radio" name="size" value="1" onChange={handleSizeChange} /> Moyen (2.5m)</label>
+            <label><input type="radio" name="size" value="4" onChange={handleSizeChange} /> Grand (4.5m)</label>
+            <label><input type="radio" name="size" value="9" onChange={handleSizeChange} /> Énorme (9m)</label>
+            <label><input type="radio" name="size" value="16" onChange={handleSizeChange} /> Gargantuesque (9m et +)</label>
             <div id="numberSelector">
               <label id="numberSelectionLabel">Nombre de Minis à intégrer :<br /><input type="number" value={repeatValue} onChange={handleRepeatChange} /></label>
             </div>
