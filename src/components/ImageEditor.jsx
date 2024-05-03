a84743731e80e78dd95099cc1a4841f18c07fa90
ArnoldMsl <arnold.masselin@gmail.com>
2024-05-03 11:42:35 +0200

    working resizing of the mini

--- src/components/ImageEditor.jsx
+++ src/components/ImageEditor.jsx
@@ -15,9 +15,9 @@ function ImageEditor({ imageUrl, onArmyChange }) {
         const baseImg = new Image();
         baseImg.onload = () => { //chargement de l'image de la base
           setBaseImg(baseImg);
-          drawCanvas(img, baseImg, selectedSize); // Appelez drawCanvas avec img et baseImg
+          drawCanvas(img, baseImg, selectedSize); //à chaque modification de l'image de base ou de la taille, drawImage est appelée
         };
-        baseImg.src = '../src/assets/images/Minibase.svg'; // Assurez-vous que le chemin est correct
+        baseImg.src = '../src/assets/images/Minibase.svg';
       };
       img.src = imageUrl;
     }
@@ -35,55 +35,57 @@ function ImageEditor({ imageUrl, onArmyChange }) {
     return { baseWidth, baseHeight }
   }
 
-
   const resizeImage = (img, baseWidth) => {
-    const aspectRatio = img.width / img.height
-    console.log(" aspect ratio : ", aspectRatio)
-    const resizedImageWidth = baseWidth
-    const resizedImageHeight = resizedImageWidth / aspectRatio
+    const aspectRatio = img.width / img.height;
+    const resizedImageWidth = baseWidth;
+    const resizedImageHeight = resizedImageWidth / aspectRatio;
 
-    return { resizedImageHeight, resizedImageWidth }
+    const canvas = document.createElement('canvas');
+    canvas.width = resizedImageWidth;
+    canvas.height = resizedImageHeight;
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0, resizedImageWidth, resizedImageHeight);
+
+    const resizedImageDataUrl = canvas.toDataURL('image/jpeg');
+
+    const resizedImage = new Image();
+    resizedImage.width = resizedImageWidth;
+    resizedImage.height = resizedImageHeight;
+    resizedImage.src = resizedImageDataUrl;
+
+    return resizedImage;
   }
 
   const drawCanvas = (img, baseImg, selectedSize) => {
 
-    console.log("largeur native : ", img.width)
-    console.log("hauteur native : ", img.height)
-    console.log("base : ", selectedSize)
-
     const { baseWidth, baseHeight } = calculateBaseSize(selectedSize)
-    const foldStroke = 4 // l'espace de pliure est défini à 4 pixels, soit environ 1mm.
+    const foldStroke = 2 // l'espace de pliure est défini à 2 pixels, soit environ 0.5mm.
 
-    const canvas = document.createElement('canvas')
-    const ctx = canvas.getContext('2d')
+    const resizedImage = resizeImage(img, baseWidth) //on créé une nouvelle image redimensionnée
 
-    const { resizedImageHeight, resizedImageWidth } = resizeImage(img, baseWidth)
-    img.height = resizedImageHeight
-    img.width = resizedImageWidth
+    resizedImage.onload = () => { //au chargement de l'image redimensionnée, on la dessine sur le canvas.
 
-    console.log("largeur recalculée : ", img.width)
-    console.log("hauteur recalculée : ", img.height)
+      const canvas = document.createElement('canvas')
+      const ctx = canvas.getContext('2d')
 
-    canvas.width = resizedImageWidth
-    canvas.height = ((img.height * 2) + (baseHeight * 2) + (foldStroke * 2))
-    const centerYCanvas = ((canvas.height - ((img.height * 2) + (foldStroke * 2))) / 2)
+      canvas.width = baseWidth
+      canvas.height = ((resizedImage.height * 2) + (baseHeight * 2) + (foldStroke * 2))
+      const centerYCanvas = ((canvas.height - ((resizedImage.height * 2) + (foldStroke * 2))) / 2)
 
-    console.log("baseWidth : ", baseWidth)
-    console.log("canvas.width : ", canvas.width)
+      ctx.save()
+      ctx.fillStyle = 'black'
+      ctx.fillRect(0, 0, canvas.width, canvas.height)
+      ctx.scale(1, -1)
+      ctx.drawImage(resizedImage, 0, (-centerYCanvas - resizedImage.height - foldStroke))
+      ctx.restore()
+      ctx.drawImage(resizedImage, 0, (centerYCanvas + resizedImage.height + foldStroke * 2))
+      ctx.drawImage(baseImg, 0, 0, canvas.width, baseHeight)
+      ctx.drawImage(baseImg, 0, (canvas.height - baseHeight + foldStroke), canvas.width, baseHeight)
 
-    ctx.save()
-    ctx.fillStyle = 'black'
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
-    ctx.scale(1, -1)
-    ctx.drawImage(img, 0, (-centerYCanvas - img.height - foldStroke))
-    ctx.restore()
-    ctx.drawImage(img, 0, (centerYCanvas + img.height + foldStroke * 2))
-    ctx.drawImage(baseImg, 0, 0, canvas.width, baseHeight)
-    ctx.drawImage(baseImg, 0, (canvas.height - baseHeight + foldStroke), canvas.width, baseHeight)
+      const dataURL = canvas.toDataURL('image/jpeg')
 
-    const dataURL = canvas.toDataURL('image/jpeg')
-
-    setImagePreview(dataURL)
+      setImagePreview(dataURL)
+    }
   }
 
 
@@ -124,12 +126,12 @@ function ImageEditor({ imageUrl, onArmyChange }) {
         <div id="armyControls">
           <div id="armyButtons">
             <h2>Choix de la taille</h2>
-            <label><input type="radio" name="size" value="0.25" onChange={handleSizeChange} /> Minuscule (1/4 carré)</label>
-            <label><input type="radio" name="size" value="1" onChange={handleSizeChange} /> Petit (1 carré)</label>
-            <label><input type="radio" name="size" value="1" onChange={handleSizeChange} /> Moyen (1 carré)</label>
-            <label><input type="radio" name="size" value="4" onChange={handleSizeChange} /> Grand (4 carrés)</label>
-            <label><input type="radio" name="size" value="9" onChange={handleSizeChange} /> Énorme (9 carrés)</label>
-            <label><input type="radio" name="size" value="16" onChange={handleSizeChange} /> Gargantuesque (16 carrés)</label>
+            <label><input type="radio" name="size" value='0.25' onChange={handleSizeChange} /> Minuscule (1/4 carré)</label>
+            <label><input type="radio" name="size" value='0.5' onChange={handleSizeChange} /> Petit (0.5 carré)</label>
+            <label><input type="radio" name="size" value='1' onChange={handleSizeChange} /> Moyen (1 carré)</label>
+            <label><input type="radio" name="size" value='4' onChange={handleSizeChange} /> Grand (4 carrés)</label>
+            <label><input type="radio" name="size" value='9' onChange={handleSizeChange} /> Énorme (9 carrés)</label>
+            <label><input type="radio" name="size" value='16' onChange={handleSizeChange} /> Gargantuesque (16 carrés)</label>
             <div id="numberSelector">
               <label id="numberSelectionLabel"><br />Nombre de Minis à intégrer :<br /><input type="number" value={repeatValue} onChange={handleRepeatChange} /></label>
             </div>
