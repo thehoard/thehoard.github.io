7892914481b033587c777eb9b9313fef5819d855
ArnoldMsl <arnold.masselin@gmail.com>
2024-07-12 11:20:11 +0200

    working V1 with static assets

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
         w-5/6 md:w-4/6 lg:w-3/6 xl:w-5/6"
       >
         <a href="https://thehoard.co/" target="_blank" className="flex mb-4 w-5/6 xl:w-2/6 2xl:w-3/6 3xl:w-3/6">
-          <img src="../src/assets/images/Th-Logo-long-blanc.png" alt="Logo The hoard" className="w-full" />
+          <img src="assets/images/Th-Logo-long-blanc.png" alt="Logo The hoard" className="w-full" />
         </a>
         <h1 className="
           text-center mainTitle
